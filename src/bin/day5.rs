46c51b901815a597082656cd1fe17497a7eeb319
Domenic Murtari <domenic@kazusan.me>
2025-12-05 19:30:33 +0900

    Day 5: Part 2
    At first tried to make a Set of all valid values. But that was going to
    take until the heat death of the universe. This is smarter.

--- src/bin/day5.rs
+++ src/bin/day5.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use std::cmp;
 use std::env;
 use std::fs;
 
@@ -29,36 +30,77 @@ fn main() {
     let mut fresh_count = 0;
 
     for id in test_ids {
-        let as_u64 = id.to_string().parse::<u64>().unwrap();
-        println!(".");
+        let as_u64 = id.to_string().parse::<i64>().unwrap();
 
         for range in &valid_id_ranges {
             if as_u64 >= range[0] && as_u64 <= range[1] {
                 fresh_count += 1;
-                println!("{} is valid", as_u64);
                 break;
             }
         }
     }
 
     println!("Total fresh: {}", fresh_count);
+
+    let all_valid_ranges = generate_all_valid(valid_ids);
+    let sum_of_range_length = all_valid_ranges.iter().fold(0, |acc, cur| {
+        return acc + (cur[1] - cur[0] + 1);
+    });
+
+    println!("Total valid IDs: {}", sum_of_range_length);
 }
 
-fn parse_valid_ranges(ranges: &Vec<&str>) -> Vec<Vec<u64>> {
+fn parse_valid_ranges(ranges: &Vec<&str>) -> Vec<Vec<i64>> {
     let mut result = Vec::new();
 
     let re = Regex::new(r"^(\d+)-(\d+)$").unwrap();
     for range in ranges {
-        println!("Range {}", range);
         let Some(caps) = re.captures(range) else {
-            println!("Failed to parse {}", range);
             continue;
         };
 
-        let start = caps[1].to_string().parse::<u64>().unwrap();
-        let end = caps[2].to_string().parse::<u64>().unwrap();
+        let start = caps[1].to_string().parse::<i64>().unwrap();
+        let end = caps[2].to_string().parse::<i64>().unwrap();
         result.push([start, end].to_vec());
     }
 
     return result;
+}
+
+fn generate_all_valid(ranges: Vec<&str>) -> Vec<Vec<i64>> {
+    let parsed = parse_valid_ranges(&ranges);
+    let mut old_parsed = parsed.clone();
+
+    loop {
+        let mut did_simplify = false;
+        let mut new_parsed: Vec<Vec<i64>> = Vec::new();
+
+        'outer: for pair in &old_parsed {
+            if new_parsed.len() == 0 {
+                new_parsed.push(pair.to_vec());
+                continue;
+            }
+
+            for existing in new_parsed.iter_mut() {
+                if cmp::max::<i64>(existing[0], pair[0]) - cmp::min::<i64>(existing[1], pair[1])
+                    <= 0
+                {
+                    existing[0] = cmp::min(existing[0], pair[0]);
+                    existing[1] = cmp::max(existing[1], pair[1]);
+                    did_simplify = true;
+                    continue 'outer;
+                }
+            }
+
+            new_parsed.push(pair.to_vec());
+        }
+
+        if !did_simplify {
+            break;
+        }
+
+        old_parsed = new_parsed;
+    }
+
+    return old_parsed;
 }
