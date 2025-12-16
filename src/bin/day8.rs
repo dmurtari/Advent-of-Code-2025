7c44cb3bbaddfac9a49e52981a6058fd6e7ce227
Domenic Murtari <domenic@kazusan.me>
2025-12-16 23:03:00 +0900

    Day 8: Part 2
    After an extended hiatus due to having no brain power left after work,
    a return to form with poorly implemented graph algorithms!

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::env;
@@ -35,6 +35,8 @@ fn main() {
         boxes.push([x, y, z].to_vec());
     }
 
+    println!("Parsed input");
+
     let mut distances: Vec<Vec<i64>> = vec![vec![i64::MAX; boxes.len()]; boxes.len()];
     for x in 0..boxes.len() {
         for y in 0..boxes.len() {
@@ -45,12 +47,18 @@ fn main() {
         }
     }
 
-    let mut ordered_indicies = Vec::new();
+    println!("Found distances");
+
+    let mut graph: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
     loop {
         let mut min = i64::MAX;
         let mut min_indicies = [0, 0];
         for i in 0..distances.len() {
             for j in 0..distances.len() {
+                if i <= j {
+                    continue;
+                }
+
                 if distances[i][j] < min {
                     min = distances[i][j];
                     min_indicies = [i, j];
@@ -58,63 +66,29 @@ fn main() {
             }
         }
 
-        ordered_indicies.push([min_indicies[0], min_indicies[1]]);
         distances[min_indicies[0]][min_indicies[1]] = i64::MAX;
 
-        if ordered_indicies.len() == boxes.len() {
+        graph
+            .entry(min_indicies[0])
+            .and_modify(|c| c.push(min_indicies[1]))
+            .or_insert([min_indicies[1]].to_vec());
+        graph
+            .entry(min_indicies[1])
+            .and_modify(|c| c.push(min_indicies[0]))
+            .or_insert([min_indicies[0]].to_vec());
+
+        let circuit = bfs(&graph, *graph.first_key_value().unwrap().0);
+        if circuit.len() == boxes.len() {
+            println!(
+                "Last min_indicies {:?}",
+                boxes[min_indicies[0]][0] * boxes[min_indicies[1]][0]
+            );
             break;
         }
     }
-
-    let mut graph: HashMap<usize, Vec<usize>> = HashMap::new();
-    for i in 0..1000 {
-        let connection = ordered_indicies[i];
-        graph
-            .entry(connection[0])
-            .and_modify(|c| c.push(connection[1]))
-            .or_insert([connection[1]].to_vec());
-        graph
-            .entry(connection[1])
-            .and_modify(|c| c.push(connection[0]))
-            .or_insert([connection[0]].to_vec());
-    }
-
-    let mut circuits = Vec::new();
-    let mut deduped = Vec::new();
-
-    for (node, _edges) in &graph {
-        circuits.push(bfs(&graph, *node));
-    }
-
-    deduped.push(&circuits[0]);
-    'outer: for candidate in &circuits {
-        for c in 0..deduped.len() {
-            if are_sets_equal(&candidate, &deduped[c]) {
-                println!("Same");
-                continue 'outer;
-            }
-        }
-        deduped.push(&candidate);
-    }
-
-    let mut sizes = deduped.iter().fold(Vec::new(), |mut acc, c| {
-        acc.push(c.len());
-        return acc;
-    });
-
-    sizes.sort();
-    sizes.reverse();
-
-    let mut product = 1;
-    for i in 0..3 {
-        product *= sizes[i];
-    }
-
-    println!("Sizes {:?}", sizes);
-    println!("Product {}", product);
 }
 
-fn bfs(graph: &HashMap<usize, Vec<usize>>, start: usize) -> HashSet<usize> {
+fn bfs(graph: &BTreeMap<usize, Vec<usize>>, start: usize) -> HashSet<usize> {
     let mut circuit = HashSet::new();
     let mut queue = VecDeque::new();
     queue.push_front(start);
@@ -139,21 +113,6 @@ fn bfs(graph: &HashMap<usize, Vec<usize>>, start: usize) -> HashSet<usize> {
     }
 
     return circuit;
-}
-
-fn are_sets_equal(set1: &HashSet<usize>, set2: &HashSet<usize>) -> bool {
-    println!("Compparing {:?}, {:?}", set1, set2);
-    if set1.len() != set2.len() {
-        return false;
-    }
-
-    for element in set1 {
-        if !set2.contains(element) {
-            return false;
-        }
-    }
-
-    return true;
 }
 
 fn distance(box1: &Vec<i64>, box2: &Vec<i64>) -> i64 {
